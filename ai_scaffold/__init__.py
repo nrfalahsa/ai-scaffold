311c1f4698e7dc198906510500edb4285b41be1d
nrfalahsa <nurfalahsyufa2@gmail.com>
2025-12-18 20:10:30 +0700

    change version

--- ai_scaffold/__init__.py
+++ ai_scaffold/__init__.py
@@ -2,7 +2,7 @@
 ai-scaffold
 Generate real project files from AI-generated Markdown or JSON.
 """
-__version__ = "1.3.0"
+__version__ = "1.0.0"
 from .parser import parse_markdown, parse_json
 from .generator import generate
 __all__ = ["parse_markdown", "parse_json", "generate", "__version__"]
